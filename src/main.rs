f2e8c3ad72bda8c8ab648b81a98bc81b2ec65315
Kevin Xiao <yunpeng.shaw@gmail.com>
2025-01-07 10:39:00 -0800

    refactorying

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use anyhow::{bail, Result};
 use std::fs::File;
 use std::io::prelude::*;
+use std::os::unix::prelude::FileExt;
 
 const FILE_HEADER_SIZE: usize = 100;
 
 struct FileHeader {
-    raw: Vec<u8>,
-    cell_count: u16,
+    page_size: u16,
 }
 
 impl FileHeader {
@@ -14,10 +14,36 @@ impl FileHeader {
         let mut header = [0; FILE_HEADER_SIZE];
         let _ = file.read_exact(&mut header);
         Self {
-            raw: header.to_vec(),
-            cell_count: u16::from_be_bytes([header[3], header[4]]),
+            page_size: u16::from_be_bytes([header[16], header[17]]),
         }
     }
+}
+
+struct PageHeader {
+    //page_type: u8,
+    //start_of_freeblock: u16,
+    cell_count: u16,
+    //start_of_cell_content: u16,
+    //fragmented_bytes: u8,
+    //page_number: u32,
+}
+
+impl PageHeader {
+    fn from(file: &mut File) -> Self {
+        let mut buffer = [0; 2];
+        let _bytes_read = file.read_at(&mut buffer, 103);
+        Self {
+            cell_count: u16::from_be_bytes([buffer[0], buffer[1]]),
+        }
+    }
+}
+
+struct CellArray {
+
+}
+
+struct Cell {
+    
 }
 
 fn main() -> Result<()> {
@@ -33,9 +59,6 @@ fn main() -> Result<()> {
     let command = &args[2];
     match command.as_str() {
         ".dbinfo" => {
-            let mut file = File::open(&args[1])?;
-            let mut header = [0; 100];
-            file.read_exact(&mut header)?;
             /*
             A b-tree page is divided into regions in the following order:
                 * The 100-byte database file header (found on page 1 only)
@@ -45,16 +68,12 @@ fn main() -> Result<()> {
                 * The cell content area
                 * The reserved region
             */
-            // The page size is stored at the 16th byte offset, using 2 bytes in big-endian order
-            #[allow(unused_variables)]
-            let page_size = u16::from_be_bytes([header[16], header[17]]);
+            let mut file = File::open(&args[1])?;
             let file_header = FileHeader::from(&mut file);
-            // You can use print statements as follows for debugging, they'll be visible when running tests.
-            eprintln!("Logs from your program will appear here!");
+            let page_header = PageHeader::from(&mut file);
 
-            // Uncomment this block to pass the first stage
-            println!("database page size: {}", page_size);
-            println!("number of tables: {}", file_header.cell_count);
+            println!("database page size: {}", file_header.page_size);
+            println!("number of tables: {}", page_header.cell_count);
         }
         _ => bail!("Missing or invalid command passed: {}", command),
     }
