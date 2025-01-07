26e9198487a518ade613607a3d0f46f00ef87704
Yunpeng Xiao <yunpeng.xiao@salesforce.com>
2025-01-07 12:01:04 -0800

    reorg

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,8 @@
+mod page;
+
 use anyhow::{bail, Result};
 use std::fs::File;
-use std::io::prelude::*;
-use std::os::unix::prelude::FileExt;
-
-const FILE_HEADER_SIZE: usize = 100;
-
-struct FileHeader {
-    page_size: u16,
-}
-
-impl FileHeader {
-    fn from(file: &mut File) -> Self {
-        let mut header = [0; FILE_HEADER_SIZE];
-        let _ = file.read_exact(&mut header);
-        Self {
-            page_size: u16::from_be_bytes([header[16], header[17]]),
-        }
-    }
-}
-
-struct PageHeader {
-    //page_type: u8,
-    //start_of_freeblock: u16,
-    cell_count: u16,
-    //start_of_cell_content: u16,
-    //fragmented_bytes: u8,
-    //page_number: u32,
-}
-
-impl PageHeader {
-    fn from(file: &mut File) -> Self {
-        let mut buffer = [0; 2];
-        let _bytes_read = file.read_at(&mut buffer, 103);
-        Self {
-            cell_count: u16::from_be_bytes([buffer[0], buffer[1]]),
-        }
-    }
-}
-
-struct CellArray {
-
-}
-
-struct Cell {
-    
-}
+use page::{PageHeader, FileHeader};
 
 fn main() -> Result<()> {
     // Parse arguments
